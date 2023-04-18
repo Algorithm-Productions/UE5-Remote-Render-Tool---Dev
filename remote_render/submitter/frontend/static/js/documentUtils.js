6a4a00a5698a02719f1eae465b89912a34499bd4
grandmen123 <vitor@bu.edu>
2023-04-18 18:10:46 +0100

    More QoL

--- remote_render/submitter/frontend/static/js/documentUtils.js
+++ remote_render/submitter/frontend/static/js/documentUtils.js
@@ -27,11 +27,48 @@ const changeCheckBox = (target, secondary='') => {
     }
 }
 
+const invertedChangeCheckBox = (target, secondary='') => {
+    const element = document.getElementById(target)
+    if (element.disabled) {
+        element.disabled = false
+
+        if (secondary !== '') {
+            document.getElementById(secondary).disabled = true
+            document.getElementById(secondary).hidden = false
+        }
+    } else {
+        element.disabled = true
+        element.value = ""
+        if (element.type === "select-one")
+            element.value = "False"
+
+        if (secondary !== '') {
+            document.getElementById(secondary).disabled = true
+            document.getElementById(secondary).hidden = false
+        }
+    }
+}
+
 const toggleValue = (event, value='', inner = '') => {
     document.getElementById(event.target.id).value = event.target.value
     if (value !== '') {
         document.getElementById(value).hidden = event.target.value === "False"
         if (inner !== '') {
+            const split = inner.split(";")
+            split.forEach((element) => {
+                document.getElementById(element).value = ""
+                document.getElementById(element).disabled = true
+                document.getElementById(element + "Flag").checked = false
+            })
+        }
+    }
+}
+
+const invertedToggleValue = (event, value='', inner = '') => {
+    document.getElementById(event.target.id).value = event.target.value
+    if (value !== '') {
+        document.getElementById(value).hidden = event.target.value === "True"
+        if (inner !== '' && event.target.value === "True") {
             const split = inner.split(";")
             split.forEach((element) => {
                 document.getElementById(element).value = ""
